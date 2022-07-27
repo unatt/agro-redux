302d9758e3b8e4326b1290ec9b524911a375360f
unatt <yurinnik@gmail.com>
2022-07-27 13:08:50 +0300

    added filter-slice reducers tests

--- src/store/filter-slice.test.ts
+++ src/store/filter-slice.test.ts
@@ -14,17 +14,19 @@ test('change search filter', () => {
 })
 
 test('toggle filter isNew status', () => {
+  const prevState = state
   const newState = filterSLice.reducer(state, filterActions.toggleFilterStatus('isNew'))
-  expect(newState.filter.isNew).toBeTruthy()
+  expect(newState.filter.isNew).toEqual(!prevState.filter.isNew)
   expect(newState.filter.isLimited).toBeFalsy()
   expect(newState.filter.category).toHaveLength(0)
   expect(newState.filter.search).toEqual('')
 })
 
 test('toggle filter isLimited status', () => {
+  const prevState = state
   const newState = filterSLice.reducer(state, filterActions.toggleFilterStatus('isLimited'))
+  expect(newState.filter.isLimited).toEqual(!prevState.filter.isLimited)
   expect(newState.filter.isNew).toBeFalsy()
-  expect(newState.filter.isLimited).toBeTruthy()
   expect(newState.filter.category).toHaveLength(0)
   expect(newState.filter.search).toEqual('')
 })
@@ -40,6 +42,15 @@ test('add filter category', () => {
 test('remove filter category', () => {
   state = { filter: { isNew: false, isLimited: false, category: ['test-category'], search: '' } }
   const newState = filterSLice.reducer(state, filterActions.changeFilterCategory('test-category'))
+  expect(newState.filter.isNew).toBeFalsy()
+  expect(newState.filter.isLimited).toBeFalsy()
+  expect(newState.filter.category).not.toContain('test-category')
+  expect(newState.filter.search).toEqual('')
+})
+
+test('clear filter', () => {
+  state = { filter: { isNew: true, isLimited: true, category: ['categoty to delete'], search: 'search to delete' } }
+  const newState = filterSLice.reducer(state, filterActions.clearFilter())
   expect(newState.filter.isNew).toBeFalsy()
   expect(newState.filter.isLimited).toBeFalsy()
   expect(newState.filter.category).toHaveLength(0)
