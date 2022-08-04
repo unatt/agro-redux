ef59c78956621afb10d8b60041416f8e48a0f691
unatt <yurinnik@gmail.com>
2022-08-04 09:55:50 +0300

    changed spinner key

--- src/components/UI/Spinner.tsx
+++ src/components/UI/Spinner.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 import styles from './Spinner.module.scss'
 
 const Spinner = () => {
-  const innerDivArray = new Array(12).fill(<div></div>)
-  return <div className={styles['lds-default']}>{innerDivArray}</div>
+  const innerDivArray = new Array(12).fill('')
+
+  return (
+    <div className={styles['lds-default']}>
+      {innerDivArray.map((_, index) => (
+        <div key={index}></div>
+      ))}
+    </div>
+  )
 }
 
 export default Spinner
