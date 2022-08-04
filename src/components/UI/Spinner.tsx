58139a094f2c471d5e314e4eaace10fc387f1010
unatt <yurinnik@gmail.com>
2022-08-04 09:24:47 +0300

    small refactoring of UI spinner code

--- src/components/UI/Spinner.tsx
+++ src/components/UI/Spinner.tsx
@@ -2,22 +2,8 @@ import React from 'react'
 import styles from './Spinner.module.scss'
 
 const Spinner = () => {
-  return (
-    <div className={styles['lds-default']}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  )
+  const innerDivArray = new Array(12).fill(<div></div>)
+  return <div className={styles['lds-default']}>{innerDivArray}</div>
 }
 
 export default Spinner
